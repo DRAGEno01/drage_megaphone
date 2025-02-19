2933a3f9a3c96d5829b1df253508922ddc9c0dd2
DRAGEno01 <73526439+DRAGEno01@users.noreply.github.com>
2025-02-20 10:34:24 +1100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DRAGEno01'
+description 'Emergency Vehicle Megaphone System'
+version '1.0.0'
+
+client_scripts {
+    'client/megaphone.lua'
+}
+
+dependencies {
+    'qb-core',
+    'pma-voice',
+    'lvc'
+} 
